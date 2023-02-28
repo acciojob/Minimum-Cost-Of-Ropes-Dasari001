223884e842a47cf47735e4deb4dd549894e3d465
Dasari001 <107811202+Dasari001@users.noreply.github.com>
2023-02-28 12:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
+function calculateMinCost(event) {
+	var inputEle = document.querySelector('input').value;
+	// alert(inputEle)
+	var arr = inputEle.split(',');
+	arr.sort(function(a,b){
+		return a-b;
+	});
+
+	var cost = 0;
+	while(arr.length>1) {
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost+=res;
+		arr.shift();
+		arr.shift();
+		arr.sort(function(a,b){
+		return a-b;
+	});
+	}
+
+	document.getElementById('result').textContent = cost
+
 }  
